72610e79524b41898b84ef0ea24f2224f3135701
jairorjuela <genius85862@gmail.com>
2018-04-01 15:02:46 -0500

    Filter

--- src/App.js
+++ src/App.js
@@ -4,18 +4,50 @@ import posts from './posts'
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      potst: posts,
+      term: ""
+    };
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+
+  searchHandler(event){
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  renderPosts() {
+    let termi = this.state.term
+     const renderPosts = posts
+     .filter(post => {
+       return post.title.toLowerCase().indexOf(termi.toLowerCase()) >= 0
+     })
+     .map((post, index) => {
+       return (
+         <li key={index}>
+          <a href={post.url}><img src={post.image} alt={post.title}/></a>
+           <p>{post.title}</p>
+         </li>
+       )
+     })
+     return renderPosts
+   }
+
   render() {
+
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.searchHandler}/>
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.renderPosts()}
         </ul>
       </div>
     )
@@ -24,5 +56,3 @@ class App extends Component {
 
 
 export default App
-
-
